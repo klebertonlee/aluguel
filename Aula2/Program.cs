af45c4f4382966264548270a2243e6eaad30c2e7
kleberton <kpleandro@hotmail.com>
2020-06-23 17:24:47 -0300

    exercicio de vetor

---
+++ Aula2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            product[] vs = new product[10];
+
+            Console.Write("Quantos quartos serão necessarios: ");
+            int quarto = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+                        
+            for(int i = 1; i <= quarto; i ++)
+            {
+                Console.WriteLine($"Aluguel #{i}: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int vago = int.Parse(Console.ReadLine());
+                vs[vago] = new product(nome, email);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quantos quartos: ");
+            for(int i = 1; i < 10; i++)
+            {
+                if (vs[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vs[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Aula2/product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula2
+{
+    class product
+    {
+        
+        public string Nome;
+        public string Email;
+
+        
+        public string nome
+        {
+            get { return Nome; }
+            set { Nome = value; }
+        }
+        public string email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public product(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " , " + Email;
+               
+        }
+    }
+}
